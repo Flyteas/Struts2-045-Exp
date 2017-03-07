cc8d84abb96bd4aa21ce2d0e0671f76480ce1c33
Flyteas <flyteas@gmail.com>
2017-03-07 20:38:40 +0800

    Add scroll and default command

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 Struts2 S2-045 (CVE-2017-5638) Exp
+
 MFC
 
 Mail: flyteas@gmail.com
--- Struts2-045-Exp/Struts2-045-ExpDlg.cpp
+++ Struts2-045-Exp/Struts2-045-ExpDlg.cpp
@@ -102,7 +102,8 @@ BOOL CStruts2045ExpDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 	// TODO: 在此添加额外的初始化代码
-
+	editTextCommand = "whoami"; //默认命令
+	UpdateData(false);
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
 
